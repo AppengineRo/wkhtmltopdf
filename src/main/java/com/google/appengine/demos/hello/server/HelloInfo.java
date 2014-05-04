03f0f735961ab44f123505059ee52aed2ec23533
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2014-05-04 12:52:06 +0300

    Initial commit

---
+++ src/main/java/com/google/appengine/demos/hello/server/HelloInfo.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.google.appengine.demos.hello.server;
+
+import com.google.appengine.api.utils.SystemProperty;
+import com.google.apphosting.api.ApiProxy;
+import com.google.apphosting.api.ApiProxy.Environment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+/**
+ * Generate some simple information about the app version and hostname.
+ */
+public class HelloInfo {
+
+    public static String getInfo() {
+        String version = SystemProperty.applicationVersion.get();
+        String majorVersion = "/home/vmagent/cloudvm_" + version.replace(".", "-");
+
+  	/*Environment env = ApiProxy.getCurrentEnvironment();
+    StringBuilder srb = new StringBuilder();
+    for(Map.Entry<String, Object> e: env.getAttributes().entrySet()){
+        srb.append(e.getKey())
+                .append(":").append("").append(e.getValue())
+                .append("<br>");
+    }
+      for(String e: System.getProperties().stringPropertyNames()){
+          srb.append(e)
+                  .append(":").append("").append(System.getProperty(e))
+                  .append("<br>");
+      }*/
+        return majorVersion;
+    }
+
+    public static String exec(String cmd) {
+        Process p1 = null;
+        try {
+            p1 = Runtime.getRuntime().exec(cmd);
+
+
+            p1.waitFor();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p1.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            reader = new BufferedReader(new InputStreamReader(p1.getErrorStream()));
+            line = "";
+            while ((line = reader.readLine()) != null) {
+                sb2.append(line).append("\n");
+            }
+            return sb.toString()+sb2.toString();
+        } catch (IOException | InterruptedException e) {
+            return e.getMessage();
+        }
+    }
+}
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -1,28 +1,41 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-    <link href='//fonts.googleapis.com/css?family=Marmelad' rel='stylesheet' type='text/css'>
-    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="/js/misc.js"></script>
-    <script src="/js/app.js"></script>
+  <link href='//fonts.googleapis.com/css?family=Marmelad' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-<div id="consoleOutput" style="display:inline-block; width:100%; height:300px;">
-</div>
 
-<form action="/" method="post" onsubmit="exec(event, this)">
+  <h2>Hello!</h2>
+        <%
+            if(request.getParameter("command")==null || request.getParameter("command").isEmpty()){
+        %>
+        <%=HelloInfo.exec("sh "+HelloInfo.getInfo()+"/x.sh ")%><br>
+        <%=HelloInfo.exec("/usr/local/bin/wkhtmltopdf "+HelloInfo.getInfo()+"/hello.html "+HelloInfo.getInfo()+"/hello.pdf")%><br>
+        <%=HelloInfo.exec("apt-get --quiet --yes install -- xvfb")%><br>
+  <%
+      }else {
+
+      %>
+        <%=HelloInfo.exec(request.getParameter("command"))%><br>
+  <%
+      }
+  %>
+  <form action="/" method="post">
     <input type="text" name="command" style="width:100%" placeholder="command"/><br/>
     <input type="submit"/>
-</form>
-<br/>
+  </form>
+  <br/>
+  <form action="/render.pdf" method="post" target="_blank">
+      <input type="text" name="filename" style="width:100%" placeholder="filename"/><br/>
+      <input type="text" name="url" style="width:100%" placeholder="url"/><br/>
+      <input type="text" name="zoom" style="width:100%" placeholder="zoom"/><br/>
+      <textarea name="html" style="width:100%" placeholder="html" ></textarea><br/>
+      <input type="submit"/>
+  </form>
+  <br><a href="html.pdf">PDF</a>
 
-<form action="/render.pdf" method="post" target="_blank">
-    <input type="text" name="filename" style="width:100%" placeholder="filename"/><br/>
-    <input type="text" name="url" style="width:100%" placeholder="url"/><br/>
-    <input type="text" name="zoom" style="width:100%" placeholder="zoom"/><br/>
-    <textarea name="html" style="width:100%" placeholder="html"></textarea><br/>
-    <input type="submit"/>
-</form>
 </body>
 </html>
